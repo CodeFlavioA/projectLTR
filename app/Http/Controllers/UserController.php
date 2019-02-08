b91caa49d200ccd1ac58b88f4338a195c315e340
Flavio A Andres <FlavioAandres24@gmail.com>
2019-02-08 00:47:04 -0500

    users ok, and auth with jwt

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use JWTAuthException;
+
+class UserController extends Controller
+{
+    private function getToken($email, $password)
+    {
+        $token = null;
+        try {
+            if (!$token = JWTAuth::attempt( ['email'=>$email, 'password'=>$password])) {
+                return response()->json([
+                    'response' => 'error',
+                    'message' => 'Password or email is invalid',
+                    'token'=>$token
+                ]);
+            }
+        } catch (JWTAuthException $e) {
+            return response()->json([
+                'response' => 'error',
+                'message' => 'Token creation failed',
+            ]);
+        }
+        return $token;
+    }
+    
+    public function login(Request $request)
+    {
+        $user = \App\User::where('email', $request->email)->get()->first();
+        if ($user && \Hash::check($request->password, $user->password)) // The passwords match...
+        {
+            $token = self::getToken($request->email, $request->password);
+            $user->auth_token = $token;
+            $user->save();
+            $response = ['success'=>true, 'data'=>['id'=>$user->id,'auth_token'=>$user->auth_token,'name'=>$user->name, 'email'=>$user->email]];
+        }
+        else
+          $response = ['success'=>false, 'data'=>'Record doesnt exists'];
+
+        return response()->json($response, 201);
+    }
+}
